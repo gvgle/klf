a621aff666afac3bb79716e10f87ed7b9c1cdd04
cleven <cleven@guagle.com>
2026-02-25 22:16:05 -0800

    移除 CNAME 以使用 GitHub Pages 默认域名

---
+++ src/App-simple-test.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full h-screen bg-green-600 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        React is working! 测试成功！
+      </h1>
+    </div>
+  );
+}
